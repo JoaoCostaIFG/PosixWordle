3e6df41dc2dc63dec31ac752bc671f30244b7948
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-02-03 16:29:36 +0000

    Ye this is the commit. This took like 10 minutes.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wordle in POSIX shell
+
+Implementation of wordle in POSIX shell (at least I think it is POSIX
+compliant). Inspired by
+[this bash implementation](https://gist.github.com/huytd/6a1a6a7b34a0d0abcac00b47e3d01513)
+of the game.
---
+++ wordle.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+max_wcnt="$(grep -c '^\w\w\w\w\w$' dict)"
+soli="$(shuf -i 0-"$max_wcnt" -n 1)"
+sol="$(grep -m "$soli" '^\w\w\w\w\w$' dict | tail -1 | tr '[:lower:]' '[:upper:]')"
+
+max_guess=6
+curr_guess=1
+while [ $curr_guess -le $max_guess ]; do
+  guess=""
+  while [ "$(printf "%s" "$guess" | wc -c)" -ne 5 ]; do
+    printf "Your guess [%d/%d]: " "$curr_guess" "$max_guess"
+    read -r guess
+  done
+  guess="$(printf "%s" "$guess" | tr '[:lower:]' '[:upper:]')"
+
+  correct=0
+  for i in 1 2 3 4 5; do
+    sol_char="$(printf "%s" "$sol" | cut "-c${i}")"
+    guess_char="$(printf "%s" "$guess" | cut "-c${i}")"
+
+    if [ "$sol_char" = "$guess_char" ]; then
+      correct=$((correct + 1))
+      printf "\033[30;102m%s\033[0m" "$guess_char"
+    elif echo "$sol" | grep -q -F "$guess_char"; then
+      printf "\033[30;103m%s\033[0m" "$guess_char"
+    else
+      printf "\033[30;107m%s\033[0m" "$guess_char"
+    fi
+  done
+  echo
+
+  if [ "$correct" -eq 5 ]; then
+    echo "You got it!"
+    curr_guess=7
+  else
+    curr_guess=$((curr_guess + 1))
+  fi
+done
+
+printf "Solution: %s\n" "$sol"
